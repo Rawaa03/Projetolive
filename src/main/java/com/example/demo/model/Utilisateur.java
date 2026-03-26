d71ae749bb59d7115a418dd7394259e8f1d44dc6
rihem yahia <yahiarihem377@gmail.com>
2026-03-26 20:57:59 +0100

    Your commit message

--- src/main/java/com/example/demo/model/Utilisateur.java
+++ src/main/java/com/example/demo/model/Utilisateur.java
@@ -14,38 +14,32 @@ import java.util.List;
 @AllArgsConstructor
 @Document(collection = "utilisateurs")
 public class Utilisateur {
-    
+
     @Id
     private String id;
-    
+
     @Indexed(unique = true)
     private String email;
-    
+
     private String motDePasse;
-    
+
     private String prenom;
-    
+
     private String nom;
-    
+
     private String telephone;
-    
-    private String role; // responsable, agriculteur, equipe_recolte, transporteur, pressoir
-    
+
+    private String role; // admin, responsable, agriculteur, equipe_recolte, transporteur, pressoir
+
     private String adresse;
-    
-    // Pour agriculteur - liste des vergers (objets complets)
-    private List<Verger> vergers;
-    
-    // Pour équipe de récolte
-    private EquipeInfo equipeInfo;
-    
-    // Pour transporteur
-    private Ressource vehicule;
-    
-    // Pour pressoir
-    private PressoirInfo pressoirInfo;
-    
+
+    private List<String> vergersIds;
+
+    private EquipeInfo infoEquipe;
+
+    private PressoirInfo infoPressoir;
+
     private Boolean estActif;
-    
+
     private Date dateCreation;
 }
